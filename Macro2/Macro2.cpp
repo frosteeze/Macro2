568938009ef8b1e82c925b5a7c93220f5c27be70
Andre Supena <kalimonton@yahoo.com>
2013-12-31 17:49:43 -0800

    Main

---
+++ Macro2/Macro2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <stdlib.h>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+		cout << "Frosteeze's autoclicker" << endl;
+        cout << " " << endl;
+        cout << " " << endl;
+        cout << " " << endl;
+
+
+		int slpTime;
+		boolean notPause = true;
+
+		cout << "Enter milliseconds in between clicks: " << endl;
+		cin >> slpTime;
+		cout << " \nTime in miliseconds between clicks: " << slpTime << endl;
+        while(notPause){
+			if(GetAsyncKeyState(VK_F10))
+			{
+				while(true)
+				{
+					if(GetAsyncKeyState(VK_F10))   
+					break;
+					if (GetAsyncKeyState(VK_F9))
+					exit(0);
+				}
+			}
+
+			else
+			{
+				mouse_event(MOUSEEVENTF_LEFTDOWN, NULL, NULL, NULL, NULL);  //Autoclicks
+				mouse_event(MOUSEEVENTF_LEFTUP, NULL, NULL, NULL, NULL);	//"Releases" mouse button
+				Sleep(slpTime);
+			}
+		}
+		return 0;
+}
